eff2e9b55757819d2e3c23ee88d05e4af9f832c0
CasperEhlin <casper.ehlin@hotmail.se>
2018-10-17 13:43:13 +0200

    #3

--- generate-token.php
+++ generate-token.php
@@ -4,4 +4,14 @@
 include "php/database.php";
     $connection = new db();
 
+    $SQL = "SELECT adminID FROM admin WHERE username = 'Casper' and password = 'Jesus'";
+    
+    $token = bin2hex(random_bytes(10));
+    $SQL="update admin set token ='$token' where admin.adminId = 1";
+            $connection->execute($SQL);
+
+    echo json_encode([
+        "status"=>true, "message"=>"Token genererad", "token"=>$token
+    ]);
+
 ?>
