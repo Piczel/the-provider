3f9937a132e47b1d7fd1fb25a82186dd415d69ea
Samuel Eckerrot <samuel.eckerrot@learnet.se>
2018-10-17 13:17:37 +0200

    Added 'either' function to utility

--- wiki/php/utility.php
+++ wiki/php/utility.php
@@ -3,7 +3,7 @@
 
     class Token
     {
-        public static function verify($adminID, $token)
+        public static function verify($service, $adminID, $token)
         {
             if(true)
             {
@@ -11,10 +11,13 @@
             }
 
             throw new Exception("Användande av felaktig token");
+
+            throw new Exception("Du har inte tillgång till denna tjänd")
         }
+
     }
-
-
+    
+    
     class Input
     {
         public static function validate(array $array, array $limits)
@@ -26,9 +29,9 @@
                     throw new Exception('Odefinierat värde: "'. $key .'"');
                 }
 
-
                 # Skip limit check
-                if(!is_numeric($limit)) {
+                if(!is_numeric($limit))
+                {
                     continue;
                 }
 
@@ -37,6 +40,31 @@
                     throw new Exception('Värdet "'. $key .'" överskrider den maximala längden ('. $limit .')');
                 }
             }
+        }
+        public static function either(array $array, array $limits)
+        {
+            # Returns the key in the provided array where the key is defined
+
+            foreach($limits as $key => $limit)
+            {
+                if(!isset($array[$key]))
+                {
+                    continue;
+                }
+
+                # If numeric, do limit check
+                if(is_numeric($limit))
+                {   
+                    if(strlen($array[$key]) > $limit)
+                    {
+                        throw new Exception('Värdet "'. $key .'" överskrider den maximala längden ('. $limit .')');
+                    }
+                }
+                
+                return $key;
+            }
+
+            throw new Exception('Inget av värdena "'. implode('", "', array_keys($limits)) .'" är definierade');
         }
     }
 ?>
