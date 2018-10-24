7d60d49cdb0289b26cdb5d7fd4db3b08b6ab1306
Piczel <eckerrot.samuel@gmail.com>
2018-10-24 18:12:33 +0200

    Added get-bookmarks.php

---
+++ wiki/php/get-bookmarks.php
@@ -0,0 +1,64 @@
+<?php
+    include_once '../../utility/utility.php';
+
+    $response = null;
+
+    try
+    {
+        # Decode the input JSON to a PHP array
+        $input = json_decode(file_get_contents('php://input'), true);
+
+        Input::validate($input, [
+            'accountID' => null,
+            'token' => 20
+        ]);
+
+        if(!Token::verify($input['accountID'], $input['token']))
+        {
+            throw new Exception('Felaktig token');
+        }
+
+        $connection = new DBConnection();
+        
+
+        # Fetches the wiki and assumes the account is either admin or wikiuser
+        # Only the one(s) with a database result is stored in $wikis
+        $wikis = array_filter([
+            $connection->query('SELECT wikiID, mayAssignAccept AS "setting" FROM wiki INNER JOIN admin_wiki ON forWikiID = wikiID INNER JOIN account ON admin_wiki.forAccountID = accountID WHERE accountID = ?', [$input['accountID']]),
+            $connection->query('SELECT wikiID, mayAssignAccept AS "setting" FROM wiki INNER JOIN wikiuser ON forWikiID = wikiID WHERE wikiuser.forAccountID = ?', [$input['accountID']])
+        ]);
+
+        if(count($wikis) < 1)
+        {
+            # None of the queries returned a result
+            throw new Exception('Ditt konto tillhör inte något wiki');
+        }
+        
+        $wiki = reset($wikis)[0];
+
+        if(count($connection->query('SELECT 1 FROM admin_wiki WHERE activated_tp = 1 AND activated_user = 1 AND forWikiID = ?', [$wiki['wikiID']])) < 1)
+        {
+            # The service is not turned on for specific wiki
+            throw new Exception('Tjänsten är inte aktiverad');
+        }
+
+        $bookmarks = $connection->query('SELECT articleID, title FROM article INNER JOIN bookmark ON forArticleID = articleID WHERE forAccountID = ?', [$input['accountID']]);
+        
+        $response = [
+            'status' => true,
+            'message' => 'Bokmärken hämtade',
+            'bookmarks' => $bookmarks
+        ];
+    } catch(Exception $exc)
+    {
+        # Catch errors and create response with 'status' set to false
+        $response = [
+            'status' => false,
+            'message' => $exc->getMessage()
+        ];
+    } finally
+    {   
+        # Print the response as JSON
+        echo json_encode($response);
+    }
+?>
