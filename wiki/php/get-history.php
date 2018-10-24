bbb31699fb084cca382af41f2cc4471bbbfad09f
Piczel <eckerrot.samuel@gmail.com>
2018-10-24 08:55:38 +0200

    History not completly done

---
+++ wiki/php/get-history.php
@@ -0,0 +1,90 @@
+<?php
+    include_once '../../utility/utility.php';
+
+    $response = null;
+
+    try
+    {
+        # Decode the input JSON to a PHP array
+        $input = json_decode(file_get_contents('../json/request/get-history.json'), true);
+
+        Input::validate($input, [
+            'wikiID' => null
+        ]);
+
+        
+
+        $connection = new DBConnection();
+
+        if(count($connection->query('SELECT 1 FROM admin_wiki WHERE activated_tp = 1 AND activated_user = 1 AND forWikiID = ?', [$input['wikiID']])) < 1)
+        {
+            # The service is not turned on for specific wiki
+            throw new Exception('Tjänsten är inte aktiverad');
+        }
+        
+        $limit = min($input['limit'] ?? 24, 24);
+        $offset = $input['offset'] ?? 0;
+
+        $date_from = $input['date-form'] ?? '1971-01-01 12:00';
+        $date_to = $input['date-to'] ?? '2038-01-01 12:00';
+
+        $result = $connection->query(
+            'SELECT
+                versionID,
+                articleID,
+                title,
+                content,
+                `date`,
+                forVersionID
+            FROM article
+                INNER JOIN articleversion
+                    ON forArticleID = articleID
+            WHERE `date` BETWEEN ? AND ?
+            LIMIT '. $offset .','. $limit,
+            [
+                $date_from,
+                $date_to,
+                $input['articleID']
+            ]
+        );
+
+
+        $articles = [];
+
+        foreach($result as $row)
+        {
+            if(!isset($articles[$row['articleID']])) $articles[$row['articleID']] = [];
+
+            $articles[$row['articleID']]['currentVersionID'] = $row['forVersionID'];
+            
+            if(!isset($articles[$row['articleID']]['history'])) $articles[$row['articleID']]['history'] = [];
+
+            $articles[$row['articleID']]['history'][] = [
+                'versionID' => $row['versionID'],
+                'date' => $row['date'],
+                'title' => $row['title'],
+                'content' => $row['content']
+            ];
+        }
+
+        var_dump($result);
+
+    
+        $response = [
+            'status' => true,
+            'message' => 'Ändringshistorik hämtad',
+            'articles' => $articles
+        ];
+    } catch(Exception $exc)
+    {
+        # Catch errors and create response with 'status' set to false
+        $response = [
+            'status' => false,
+            'message' => $exc->getMessage()
+        ];
+    } finally
+    {   
+        # Print the response as JSON
+        echo json_encode($response);
+    }
+?>
