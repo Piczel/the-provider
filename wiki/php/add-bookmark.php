27fd8ccc89c3806c4711268d8855d4b67d02d10c
Piczel <eckerrot.samuel@gmail.com>
2018-10-25 09:18:24 +0200

    Updated add-bookmark.php

--- wiki/php/add-bookmark.php
+++ wiki/php/add-bookmark.php
@@ -20,7 +20,32 @@
 
         $connection = new DBConnection();
         
-        # Code...
+        # Fetches the wiki and assumes the account is either admin or wikiuser
+        # Only the one(s) with a database result is stored in $wikis
+        $wikis = array_filter([
+            $connection->query('SELECT wikiID, mayAssignAccept AS "setting" FROM wiki INNER JOIN admin_wiki ON forWikiID = wikiID INNER JOIN account ON admin_wiki.forAccountID = accountID WHERE accountID = ?', [$input['accountID']]),
+            $connection->query('SELECT wikiID, mayAssignAccept AS "setting" FROM wiki INNER JOIN wikiuser ON forWikiID = wikiID WHERE wikiuser.forAccountID = ?', [$input['accountID']])
+        ]);
+
+        if(count($wikis) < 1)
+        {
+            # None of the queries returned a result
+            throw new Exception('Ditt konto tillhör inte något wiki');
+        }
+        
+        $wiki = reset($wikis)[0];
+
+        if(count($connection->query('SELECT 1 FROM admin_wiki WHERE activated_tp = 1 AND activated_user = 1 AND forWikiID = ?', [$wiki['wikiID']])) < 1)
+        {
+            # The service is not turned on for specific wiki
+            throw new Exception('Tjänsten är inte aktiverad');
+        }
+
+        if(count($connection->query('SELECT 1 FROM article WHERE forWikiID = ? AND articleID = ?', [$wiki['wikiID'], $input['articleID']])) < 1)
+        {
+            throw new Exception('Artikeln hittades inte i ditt wiki');
+        }
+
         $SQL = 'INSERT INTO bookmark (forAccountID, forArticleID) VALUES (?, ?)';
         if(!$connection->execute($SQL, [$input['accountID'], $input['articleID']])){
             throw new Exception('Bokmärke kunde inte skapas');
