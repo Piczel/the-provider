925b1917acf5378bfc90d3a2d67887d39c9b7a80
Arkel Dagsberg <arkel.dagsberg@learnet.se>
2018-10-26 12:26:49 +0200

    updaterade addscore

--- game/php/addscore.php
+++ game/php/addscore.php
@@ -4,7 +4,7 @@
         include "../../utility/utility.php";
         $connection = new DBConnection();
 
-        $addscore = $input["score"];
+        $score = $input["score"];
         $date = $input["date"];
         $forGameID = $input["forGameID"];
         $forPlayerID= $input["forPlayerID"];
@@ -15,8 +15,8 @@
             throw new Exception("spelet är inte aktiverat");
         }
 
-        $sql = "INSERT INTO score (score, `date`,forPlayerID,forGameID) VALUES (?,?,?,?)";
-        if($connection->execute($sql, [$addscore,$date,$forGameID,$forPlayerID]) === false){
+        $sql = "INSERT INTO score (score,`date`,forGameID,forPlayerID) VALUES (?,?,?,?)";
+        if($connection->execute($sql, [$score,$date,$forGameID,$forPlayerID]) === false){
             throw new Exception("Kunde inte lägga till poäng");
         }
 
