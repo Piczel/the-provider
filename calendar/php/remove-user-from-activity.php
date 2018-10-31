2c56d15463d183c1a684fc91e80afb5490a72b39
Pedram <pedramfattahi@outlook.com>
2018-10-31 10:57:58 +0100

    removed ! from SQL

--- calendar/php/remove-user-from-activity.php
+++ calendar/php/remove-user-from-activity.php
@@ -22,7 +22,7 @@
             throw new Exception('Du har ingen aktiverad kalender');
         }
 
-        if(count($connection->query("SELECT 1 FROM calendar_activity WHERE forCalendarID != ? AND forActivityID != ?", [$calendar[0]['forCalendarID'], $input['activityID']])) < 1)
+        if(count($connection->query("SELECT 1 FROM calendar_activity WHERE forCalendarID = ? AND forActivityID = ?", [$calendar[0]['forCalendarID'], $input['activityID']])) < 1)
         {
             throw new Exception('Du är inte deltagare i denna aktivitet');
         }
